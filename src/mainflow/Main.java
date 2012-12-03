ac85bc3c997eb8722fdff42097631aae38550fd5
Aha <tzl77258511@gmail.com>
2012-12-03 19:35:35 +0800

    just change the input method

--- src/mainflow/Main.java
+++ src/mainflow/Main.java
@@ -19,11 +19,12 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		System.out.println("N皇后问题[管道过滤器风格](只能处理1-64皇后,输入-1结束程序),请输入N:");
 		int n = -1;
 	    long tm;
 	    Scanner scanner = new Scanner(System.in);
 	    while (true) {
-	        System.out.println("N皇后问题(只能处理1-64皇后,输入-1结束程序),请输入N:");
+	    	System.out.println("请输入N:");
 	        Counter.reset();
 	        n = scanner.nextInt();
 	        
@@ -42,7 +43,8 @@ public class Main {
 	        	System.out.println(" ֻ只能计算1-64之间");
 	            return ;
 	        }
-	        System.out.println(n + " 皇后");
+	        System.out.println("正在计算...");
+	        
 	        // N个皇后只需N位存储，N列中某列有皇后则对应bit置1
 	        Upperlim.reset(n);
 	        
